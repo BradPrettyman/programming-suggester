52615509d53a2fd4ad2482e3c7178d6d613356b7
Brad Prettyman <brad.prettyman5@gmail.com>
2021-08-13 12:30:12 -0700

    add more button functions

--- js/scripts.js
+++ js/scripts.js
@@ -11,3 +11,21 @@ $(document).ready(function() {
     $(".btn-next2").fadeIn();
     $(".btn-next1").fadeOut();
   });
+  
+  $(".btn-next2").click(function() {
+    $("#group3").fadeIn();
+    $(".btn-next3").fadeIn();
+    $(".btn-next2").fadeOut();
+  });
+
+  $(".btn-next3").click(function() {
+    $("#group4").fadeIn();
+    $(".btn-next4").fadeIn();
+    $(".btn-next3").fadeOut();
+  });
+
+  $(".btn-next4").click(function() {
+    $("#group5").fadeIn();
+    $(".btn-next5").fadeIn();
+    $(".btn-next4").fadeOut();
+  });
